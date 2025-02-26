dde0d3bdbeb8bcf582441282698e01be441e6b3f
dch66 <15535517032@163.com>
2025-02-26 14:36:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 
@@ -13,8 +14,8 @@ use std::io::prelude::*;
 struct Args {
     #[arg(help = "The pattern to search for")]
     pattern: String,
-    #[arg(help = "File to search")]
-    input: String,
+    #[arg(help = "File to search", required = false)]
+    input: Option<String>,
 }
 
 fn main() {
@@ -23,9 +24,20 @@ fn main() {
     let input = args.input;
 
     let re = Regex::new(pattern.as_str()).unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
 
+    if let Some(input) = input {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader,re);
+    } else { 
+        println!("请输入要查找的文本");
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader,re);
+    }
+}
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
     for line_ in reader.lines() {
         if let Ok(line) = line_ {
             let contains_substring = re.find(&line);
