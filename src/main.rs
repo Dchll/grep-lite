229bd13e64ae9198f4106efd52e34c35fd6f58cc
dch66 <15535517032@163.com>
2025-02-26 13:15:44 +0800

    clap added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use clap::Parser;
 use regex::Regex;
 
+#[derive(Parser, Debug)]
+#[command(
+    version = "0.0.1",
+    about = "searches for patterns",
+    long_about = "by 多吃韩66"
+)]
+struct Args {
+    #[arg(help = "The pattern to search for")]
+    pattern: String,
+}
+
 fn main() {
-    let re = Regex::new("picture").unwrap();
+    let args = Args::parse();
+    let pattern = args.pattern;
+
+    let re = Regex::new(pattern.as_str()).unwrap();
 
     let quote = "Every face, every shop, bedroom window, public-house, and 
 dark square is a picture feverishly turned--in search of what?
@@ -11,7 +26,7 @@ it is the same with books. What do we seek through millions of pages?";
         let contains_substring = re.find(line);
         match contains_substring {
             None => (),
-            Some(_) => println!("{}",line),
+            Some(_) => println!("{}", line),
         }
     }
 }
